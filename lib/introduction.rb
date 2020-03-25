5906d4a67b34105210a86424267ca164efd221a2
aurel fontaine <ajfame11@gmail.com>
2020-03-25 01:34:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
 def introduction(name)
-  puts "hi, my name is #{name}.
+  puts "Hi ny mae is #{name}."
+end
+def introduction_with_language(name, language)
+  puts "hi, my name is #{name} and I am learning to program in #{language}."
 end
